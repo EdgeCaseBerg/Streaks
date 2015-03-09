98224380064573bac6c39aa11e7f8b5991091c41
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-03-08 23:08:27 -0400

    Created First Simple View and asset
    It's import to note you need `import play.api._` to use the views object
    when selecting the template for a controller response.
    
    Another thing to note is `@routes.Assets.at` to retrieve the URL for
    your assets, in order for this to compile you need to have the following
    in your routes file:
    
    	GET /assets/*file controllers.Assets.at(path="/public", file)
    
    These are things which the documentation doesn't make perfectly clear,
    it might be good to write up a beginners blog post about this I think.

--- app/controllers/Streaks.scala
+++ app/controllers/Streaks.scala
@@ -1,11 +1,12 @@
 package controllers
 
+import play.api._
 import play.api.mvc._
 
 class Streaks extends Controller {
 	
 	def index = Action {
-		Ok("tada")
+		Ok(views.html.index("Streaks - Home"))
 	}
 
 }
