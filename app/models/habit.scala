c70678b51498100253dfba1f663929b9f0298725
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-03-10 07:22:45 -0400

    Writing out some ideas

--- app/models/habit.scala
+++ app/models/habit.scala
@@ -3,6 +3,10 @@ package models
 class Habit(val name: String, val daysDone: List[Int]) {
 	def computeStreak(): Int = {
 		/* iterate over daysDone and look for gaps and such */
-		-1
+		val start = 0
+		val end = 0
+		for( i <- 0 until daysDone.length) {
+			
+		}
 	}
 }
