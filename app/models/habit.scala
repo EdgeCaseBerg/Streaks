4746b5a5ca4b698a8b03a13defb6a87c43a7875c
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-03-10 23:08:35 -0400

    computeStreak function
    Created the computeStreak function O(n) time in relation to the size of
    the streak. Feels rather imperative though, kinda thinking there must be
    a way to make this more idiomatic or functionally styled.
    
    Perhaps @gfritz or @scperkins have suggestions.

--- app/models/habit.scala
+++ app/models/habit.scala
@@ -3,10 +3,24 @@ package models
 class Habit(val name: String, val daysDone: List[Int]) {
 	def computeStreak(): Int = {
 		/* iterate over daysDone and look for gaps and such */
-		val start = 0
-		val end = 0
-		for( i <- 0 until daysDone.length) {
-			
+		val sorted = daysDone.sortWith(_ < _)
+		var streak = 0
+		var topStreak = 0
+		for( i <- 0 until daysDone.length-1) {
+			if (sorted(i) - sorted(i+1) == -1) {
+				streak += 1
+			} else {
+				if (streak > topStreak) {
+					topStreak = streak + 1 //+1 to count the last day in streak before it was broken
+				}
+				streak = 0
+			}
+			println(s"$topStreak and $streak")
 		}
+		/* Don't forget to count an ongoing streak */
+		if (streak > topStreak) {
+			topStreak = streak +1
+		}
+		topStreak
 	}
 }
